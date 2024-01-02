7e5e07a8fcb4a5ddfd708ca73e4bb669bcaf8942
Kirill Svitsov <user@MacBook-Air-2.local>
2024-01-02 13:49:18 +0300

    Second commit

--- ishop/main/views.py
+++ ishop/main/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def index(request):
     context = {
         'title': 'Main',
-        'content': 'Main page of ishop'
+        'content': 'Test git checkout commit version'
     }
     return render(request, 'main/index.html', context=context)
 
