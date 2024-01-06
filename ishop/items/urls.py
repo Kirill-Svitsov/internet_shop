1b42eaa3528ce37ed7412352f38907f08865dad3
Kirill Svitsov <user@MacBook-Air-2.local>
2024-01-06 20:26:24 +0300

    Added Q search

--- ishop/items/urls.py
+++ ishop/items/urls.py
@@ -5,6 +5,7 @@ from items import views
 app_name = 'items'
 
 urlpatterns = [
+    path('search/', views.catalog, name='search'),
     path('<slug:category_slug>/', views.catalog, name='index'),
     path('item/<slug:item_slug>/', views.item, name='item'),
 ]
--- ishop/items/views.py
+++ ishop/items/views.py
@@ -2,16 +2,26 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
 
 from items import models
+from items.utils import q_search
 
-ITEMS_ON_PAGE = 3
+ITEMS_ON_PAGE = 6
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
+    page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
     if category_slug == 'all':
         items = models.Item.objects.all()
+    elif query:
+        items = q_search(query)
     else:
         items = get_list_or_404(models.Item, category__slug=category_slug)
-    page = request.GET.get('page', 1)
+    if on_sale:
+        items = items.filter(discount__gt=0)
+    if order_by and order_by != "default":
+        items = items.order_by(order_by)
     paginator = Paginator(items, ITEMS_ON_PAGE)
     current_page = paginator.page(int(page))
     context = {
