5174b95a2bfec585d45599d73c3616c12c6dd58d
Kirill Svitsov <user@MacBook-Air-2.local>
2024-01-19 15:43:17 +0300

    Added requirements and databases

--- ishop/ishop/settings.py
+++ ishop/ishop/settings.py
@@ -73,13 +73,25 @@ WSGI_APPLICATION = 'ishop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'cuprumelement29',
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
